bc121a88d2105ca044ad5294ed6231a1057ab661
ZornStolz <juan.vera1@correo.icesi.edu.co>
2021-11-30 17:06:03 -0500

    CRUD for user working

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -5,13 +5,30 @@ const UserController = require('../controller/userController')
 const SemesterController = require('../controller/semesterController')
 const CourseController = require('../controller/courseController')
 
-/* User API */
+                            /* User API */
+
+/* GET: get all users*/
 router.get('/', UserController.findAll)
 
-/* Semester API */
+/* GET: get an user by id */
+router.get('/:_id', UserController.findbyId);
+
+/* POST: add a new user uses the body*/
+router.post('/', UserController.create)
+
+/* PUT: update an user by mail, uses the body*/
+router.put('/:_id', UserController.update)
+
+/* DELETE: deletes an user by mail*/
+router.delete('/:_id', UserController.delete)
+
+                            /* Semester API */
+
+/* GET: get all semesters*/
 router.get('/semesters', SemesterController.findAll)
 
-/* Course API*/
+                            /* Course API*/
+/* GET: get all courses*/
 router.get('/semesters/courses', CourseController.findAll)
 
 module.exports = router;
