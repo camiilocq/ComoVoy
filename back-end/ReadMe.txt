70fa5e388bc9b7bc0ab716c7258fed26fd2e2027
ZornStolz <juan.vera1@correo.icesi.edu.co>
2021-12-09 19:26:45 -0500

    only front for deployment

--- back-end/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-Acá esta el back-end de la aplicación.
-
-El backend será desarrollado en Nodejs.
-
-Para desarrollo se recomienda hacer npx nodemon, y ejecutar con nodemon.
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://como-voy-back.herokuapp.com/",
   responseType: "json",
   headers: { "X-Requested-With": "XMLHttpRequest" },
 });
