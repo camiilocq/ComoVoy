6b59fc0536275ceeb60191e9c4c151c335bbab6f
ZornStolz <juan.vera1@correo.icesi.edu.co>
2021-12-09 20:32:24 -0500

    express and config on index to serve app

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import "./index.css";
 import "antd/dist/antd.css";
 import { AppContextWrapper } from "./store/AppContext";
 
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
 ReactDOM.render(
   <AppContextWrapper>
     <App />
